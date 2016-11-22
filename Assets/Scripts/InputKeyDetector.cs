347aa96b8fb9f0dfc45a6d3a6079fdf3d5e8af46
Nihil Aleph <nihilaleph@yahoo.com>
2016-11-22 18:00:16 -0200

    just update commit

---
+++ Assets/Scripts/InputKeyDetector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputKeyDetector : MonoBehaviour {
+
+    public GlowingLight SelfLight;
+    public string KeyName;
+    private bool mActive = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!mActive)
+        {
+
+            if (Input.GetKeyDown(KeyName))
+            {
+                SelfLight.SetGlowing(true);
+                mActive = true;
+            }
+        }
+	}
+}
